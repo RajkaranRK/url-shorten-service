33016e45a98ace472fb148dec61e37637a600582
Raj Karan <raj.karan@africa.airtel.com>
2023-03-05 09:01:46 +0530

    intital code setup

---
+++ src/test/java/com/rk/uss/UrlShortenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.uss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
